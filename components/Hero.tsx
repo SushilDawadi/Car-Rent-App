04a8fc01a8e9c3deed93b86c5698cbbe51d8e1d0
Sushil <sushildawadi14@gmail.com>
2023-10-09 08:43:04 +0545

    navbar footer design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Image from "next/image";
 import { CustomButton } from ".";
 const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -12,7 +14,17 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyle="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
